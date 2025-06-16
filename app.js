973aa0b153c0d0d25fb934ce3971aefc14261175
awall5 <awalmanga25@gmail.com>
2025-06-16 13:36:44 +0530

    MERN-First

---
+++ app.js
@@ -0,0 +1,27 @@
+window.console.log("Hello awal");
+
+// console.log(block);
+
+// let age = 22;
+
+// const year = 2025;
+
+
+// var block = 33; // declared
+// console.log(age,year,block);
+
+// //var superpowers 
+// var block = 55; // re-declared OR reasign
+
+// console.log(age,year,block);
+// ------------------------------------------------
+
+// console.log(age);
+let age  = 22;
+console.log(age);
+
+let a = 1;
+let b = 'a';
+
+console.log(b*a);
+
