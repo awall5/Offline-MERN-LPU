6bed4397970a5c51052066dfab57c5cc69c98ac2
awall5 <awalmanga25@gmail.com>
2025-06-18 10:39:38 +0530

    Day-3

--- Day-2/Part-1/arrays/app.js
+++ Day-2/Part-1/arrays/app.js
@@ -1,65 +1,129 @@
 
-//----------------OBJECT----------------
+// //----------------OBJECT----------------
 
-// const cse015 ={
-//     name: "Awal Manga",
-//     address: "Meerut",
-// };
+// // const cse015 ={
+// //     name: "Awal Manga",
+// //     address: "Meerut",
+// // };
 
-// console.log(cse015);
-
-
-// // To read the particular value 
-
-// const name = cse015.name;
-
-// // To update a particular value
-
-// cse015.address = "Haryana";
-
-// //Create a new value pair
-
-// cse015.Branch = "CSE";
-
-// //delete value pair
-
-// delete cse015.Branch;
-
-// //__________________________________________________________________________________---
+// // console.log(cse015);
 
 
-const person = {
-    name: 'AWAL MANGA',
-    height: 1.55,
-    college: 'LPU',
-    weight: 65,
-    roll: '14',
-    getBMI: function () {
-        const BMI=(this.weight / this.height ** 2);
-        if (BMI< 18) {
-            console.log('Below AVG');
-        }
-        else if (BMI >= 18) {
-            console.log("Normal");
-        }
+// // // To read the particular value 
+
+// // const name = cse015.name;
+
+// // // To update a particular value
+
+// // cse015.address = "Haryana";
+
+// // //Create a new value pair
+
+// // cse015.Branch = "CSE";
+
+// // //delete value pair
+
+// // delete cse015.Branch;
+
+// // //__________________________________________________________________________________---
+
+
+// const person = {
+//     name: 'AWAL MANGA',
+//     height: 1.55,
+//     college: 'LPU',
+//     weight: 65,
+//     roll: '14',
+//     getBMI: function () {
+//         const BMI=(this.weight / this.height ** 2);
+//         if (BMI< 18) {
+//             console.log('Below AVG');
+//         }
+//         else if (BMI >= 18) {
+//             console.log("Normal");
+//         }
+//     }
+// }
+
+// // person.getBMI();
+// // const allkeys = Object.keys(person);
+// // console.log(allkeys);
+// // const allvalues = Object.values(person);
+// // console.log(allvalues);
+// // const allentries = Object.entries(person);
+// // console.log(allentries);
+
+
+
+
+// const whichkey = prompt("Which key you want to see ?");     //Isse pucha ja skta hai ki kya chaiye user ko pop up ata hai
+// console.log(person[whichkey]);
+
+// //--------------DE-Structuring-------------
+// const {weight , college} = person;
+// console.log("❤️-->",weight,college);
+
+
+// const arr = ["Awal","Chirag","Lol"];
+// console.log(arr);
+
+
+// //REad a single value 
+// console.log(arr[0]);
+
+// const [a,b,c] = arr;
+// console.log(b,c);
+
+// // arr[10] = "Honda";
+// arr.push("Honda");  // add element in last 
+
+// arr.pop();  // take the element from last
+
+// arr.shift();    // It removes the element from front
+
+// arr.unshift('BMW','MERCEDES');  // It add elements from front
+
+// console.log(arr);
+
+// arr.splice(1,2,"Bugati",'Rolls Royas');     //Particular element p jake delete krta hai
+
+// console.log(arr.indexOf("Bugati"));
+
+
+// const person = {
+//     name: 'AWAL MANGA',
+//     height: 1.55,
+//     college: 'LPU',
+//     weight: 65,
+//     roll: '14',
+// }
+
+// const person2 = person;
+// const per = {...person};        // agar {} ye use krenge toh bhai address change hojyega or fir vo equal nai honge 
+
+// if(person==per){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+//-------------------------------------------------------------------
+
+
+const arr= [
+        {name: "Awal Manga" , city: "Meerut"},
+        {name: "Chirag", city: "Jalandhar"},
+];
+
+const func = (a)=>{
+    if (a.name === "Awal Manga"){
+        return {...a, remark: "Yes"};
+    }
+    else{
+       return {...a, remark: "No"};
     }
 }
-
-// person.getBMI();
-// const allkeys = Object.keys(person);
-// console.log(allkeys);
-// const allvalues = Object.values(person);
-// console.log(allvalues);
-// const allentries = Object.entries(person);
-// console.log(allentries);
-
-
-
-
-const whichkey = prompt("Which key you want to see ?");     //Isse pucha ja skta hai ki kya chaiye user ko pop up ata hai
-console.log(person[whichkey]);
-
-//--------------DE-Structuring-------------
-const {weight , college} = person;
-console.log("❤️-->",weight,college);
+const ele  = arr.map(func);
+console.log(ele);
 
---
+++ Day-3/IterationofArray/app.js
@@ -0,0 +1,20 @@
+const arr = ["cat", "Rat", "sat", "Bat"];
+
+// for(let i=0;i<arr.length;i++){
+//     const ele = arr[i];
+//     console.log(i,arr[i]);
+// }
+
+// for(let i in arr){
+//     //All keys one-by-one(avoid)
+//     const ele = arr[i];
+//     console.log(i,arr[i]);
+// }
+
+// for(let i of arr){
+//     //All keys one by one but without giving index(avoid)
+//     console.log(i);
+// }
+
+// arr.forEach(function () {});
+
