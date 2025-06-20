6dbcc15723368dbffa352b76677b899262fb9de5
awall5 <awalmanga25@gmail.com>
2025-06-20 13:12:48 +0530

    Day-5

---
+++ Day-5/app.js
@@ -0,0 +1,11 @@
+const eng = 72;
+const math=44;
+const testname = 'Mid-Sem';
+const username = 'Awal';
+
+const res = 'Got  a total marks '+(eng+math)+' in ' + testname + ' And name of student is '+username;
+
+const res2 = ` ${username} Got total marks = ${eng + math} in ${testname}`;
+
+
+console.log(res2);
