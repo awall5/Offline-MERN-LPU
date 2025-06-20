f52ecc5073d7c58b736e08e0c98e6cb9759f18dc
awall5 <awalmanga25@gmail.com>
2025-06-20 13:12:38 +0530

    async

---
+++ Day-5/async/app.js
@@ -0,0 +1,14 @@
+console.log("Start");
+
+const timeDelayInMS = 9999;
+
+const callbackfunc =() =>{
+    console.log("HEllo");
+}
+
+
+
+
+setTimeout(callbackfunc,timeDelayInMS);
+
+console.log("END");
