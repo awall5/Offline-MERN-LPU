cc05032d18d63bf5df618d78cfcc7dec7d5ad4e5
awall5 <awalmanga25@gmail.com>
2025-06-24 11:39:54 +0530

    React

---
+++ Day-7/app.js
@@ -0,0 +1,17 @@
+console.log("Hello From React");
+
+// const item1 = React.createElement("li", {}, "Item 1");
+// const item2 = React.createElement("li", {}, "Item 2");
+// const list = React.createElement("ul", {}, [item1, item2]);
+
+const list2 = (
+  <ul>
+    <li> Item1 </li>
+    <li> Item1 </li>
+  </ul>
+);
+
+const root = document.getElementById("parent");
+
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(list2);
