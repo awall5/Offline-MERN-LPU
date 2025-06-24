6bfad317769c30fef20f46488c9f3852edaf5c6b
awall5 <awalmanga25@gmail.com>
2025-06-24 14:49:02 +0530

    props

--- Day-7/basic/app.js
+++ Day-7/basic/app.js
@@ -33,35 +33,91 @@
 // reactRoot.render(container);
 
 //------------------------------------
+// const root = document.getElementById("parent");
+// const reactRoot = ReactDOM.createRoot(root);
+
+// const card = (tittle) => {
+//   return (
+//     <div className="card">
+//       <h1>{tittle}</h1>
+//       <p>Discription</p>
+//     </div>
+//   );
+// };
+
+// const Card2 = (obj) => {
+//   return (
+//     <div className="card">
+//       <h1>{obj.tittle}</h1>
+//       <p>Discription</p>
+//     </div>
+//   );
+// };
+
+// const container = (
+//   <div>
+//     {card("Hello")}
+//     {card("Hello Brother")}
+//     {card("Hey bhai")}
+//     <Card2 tittle="Hello Miss"></Card2>
+//     <Card2 tittle="Hnji"></Card2>
+//   </div>
+// );
+
+// reactRoot.render(container);
+
+// --------------------------------------------------------
+
 const root = document.getElementById("parent");
 const reactRoot = ReactDOM.createRoot(root);
 
-const card = (tittle) => {
+const arr = [
+  {
+    name: "Awal Manga",
+    age: 44,
+  },
+  {
+    name: "Mishi",
+    age: 49,
+  },
+  {
+    name: "Komu",
+    age: 55,
+  },
+  {
+    name: "Chirag",
+    age: 89,
+  },
+];
+
+const styObj = {
+  color: "pruple",
+  fontSize: "1.5rem",
+  fontWeight: "600",
+};
+
+const Card = (props) => {
+  const { name, age } = props;
   return (
     <div className="card">
-      <h1>{tittle}</h1>
-      <p>Discription</p>
+      <h1 style={{ color: "red" }}>{name}</h1>
+      <p style={styObj}>{age}</p>
     </div>
   );
 };
 
-const Card2 = (obj) => {
+const app = () => {
   return (
-    <div className="card">
-      <h1>{obj.tittle}</h1>
-      <p>Discription</p>
+    <div className="parent">
+      <h2>Hello</h2>
+      {/* <Card name={arr[0].name} age={arr[0].age}></Card>
+       */}
+
+      {arr.map((ele) => {
+        return <Card name={ele.name} age={ele.age}></Card>;
+      })}
     </div>
   );
 };
 
-const container = (
-  <div>
-    {card("Hello")}
-    {card("Hello Brother")}
-    {card("Hey bhai")}
-    <Card2 tittle="Hello Miss"></Card2>
-    <Card2 tittle="Hnji"></Card2>
-  </div>
-);
-
-reactRoot.render(container);
+reactRoot.render(app());
